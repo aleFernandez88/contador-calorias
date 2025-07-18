8e798e429f738a0ed0db4bc661a085c91fbda8fb
Ale Fernández <alejandro.d.fernandez.88@gmail.com>
2025-07-18 17:59:02 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-7xl font-bold text-blue-700'>Hola locoo.</h1>
+    </>
+  )
+}
+
+export default App
